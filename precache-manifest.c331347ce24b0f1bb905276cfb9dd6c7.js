cf5e9e81e2e8507ac5331c0c30a62a0d8b089c62
eric <ericgip97@gmail.com>
2020-07-06 05:00:29 -0700

    Updates

---
+++ precache-manifest.c331347ce24b0f1bb905276cfb9dd6c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6080cab54afe0f906fc1c5c70f71672",
+    "url": "/EricGip/LolTierListGen/index.html"
+  },
+  {
+    "revision": "3929463b6091f66c1610",
+    "url": "/EricGip/LolTierListGen/static/js/2.6ddf32bc.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/EricGip/LolTierListGen/static/js/2.6ddf32bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae2a96ccbb73d03e8fe",
+    "url": "/EricGip/LolTierListGen/static/js/main.a9694fd3.chunk.js"
+  },
+  {
+    "revision": "29825fd7e737fb64366f",
+    "url": "/EricGip/LolTierListGen/static/js/runtime-main.879b2a7a.js"
+  }
+]);
